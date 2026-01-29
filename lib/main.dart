5abf0f4c84ea6a72513f1870423aa5b7ba3e00be
Corentin Jéré <corentinjere@gmail.com>
2026-01-29 08:47:54 +0100

    Resolves 1

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+
       ),
       home: const Game(title: 'Flutter Demo Home Page'),
     );
